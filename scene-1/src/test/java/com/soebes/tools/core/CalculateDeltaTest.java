3b88e59b3f08de8fd95b89156b26df467d9833e9
Karl Heinz Marbaise <info@soebes.de>
2012-06-22 17:09:15 +0200

    - Added Scene-2

---
+++ scene-1/src/test/java/com/soebes/tools/core/CalculateDeltaTest.java
@@ -0,0 +1,28 @@
+package com.soebes.tools.core;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.testng.annotations.Test;
+
+import com.soebes.tools.core.CalculateDelta;
+
+public class CalculateDeltaTest {
+
+    @Test
+    public void deltaInMilliSecondsTest() {
+	CalculateDelta calculate = new CalculateDelta();
+	String startDate = "23.03.2010 13:10:00";
+	String endDate = "23.03.2010 14:00:00";
+	long delta = calculate.deltaInMilliSeconds(startDate, endDate);
+	assertThat(delta).isEqualTo(3000000);
+    }
+
+    @Test
+    public void deltaInSecondsTest() {
+	CalculateDelta calculate = new CalculateDelta();
+	String startDate = "23.03.2010 13:10:00";
+	String endDate = "23.03.2010 14:00:00";
+	long delta = calculate.deltaInSeconds(startDate, endDate);
+	assertThat(delta).isEqualTo(3000);
+    }
+}
